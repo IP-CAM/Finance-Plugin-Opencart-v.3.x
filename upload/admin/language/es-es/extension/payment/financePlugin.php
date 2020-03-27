1891f60265a9cd646c4b38c0e0483051ae2beb74
andivido <andrew.smith@divido.com>
2020-03-27 10:43:14 +0000

    Adds Spanish translations to the plugin

---
+++ upload/admin/language/es-es/extension/payment/financePlugin.php
@@ -0,0 +1,57 @@
+<?php
+
+$_["api_key_label"] = "Clave API";
+$_["shared_secret_label"] = "Clave secreta";
+$_["checkout_title_label"] = "Título de pago";
+$_["checkout_description_label"] = "Descripción del pago";
+$_["cart_threshold_label"] = "Umbral del carro";
+$_["show_widget_label"] = "Mostrar widget";
+$_["widget_minimum_label"] = "Mínimo de widget";
+$_["widget_minimum_description"] = "Solo muestre el widget si el precio del producto supera la cantidad especificada";
+$_["show_widget_description"] = "Muestre un útil widget con las opciones de financiación en su página de productos";
+$_["api_key_description"] = "La clave API debe ser proporcionada por su proveedor de finanzas";
+$_["shared_secret_description"] = "El secreto compartido opcional es un código acordado que se utiliza para verificar la información entre usted y su proveedor de finanzas.";
+$_["widget_prefix_label"] = "Prefijo de widget";
+$_["widget_suffix_label"] = "Sufijo de widget";
+$_["debug_label"] = "Depurar";
+$_["activate_on_status_label"] = "Activar orden en estado";
+$_["activate_on_status_description"] = "Notifique a su proveedor de finanzas que la transacción se ha completado";
+$_["limit_plans_label"] = "Planes de límite";
+$_["limit_plans_description"] = "Elija los planes que desea poner a disposición de sus clientes.";
+$_["cart_threshold_description"] = "El monto mínimo necesario para que el carrito totalice para que las finanzas sean una opción al momento de pagar";
+$_["widget_prefix_description"] = "Texto que precede al precio por mes de financiación en la página del producto";
+$_["plugin_active_label"] = "Activo";
+$_["checkout_title_description"] = "El nombre de la opción de pago al finalizar la compra";
+$_["product_selection_label"] = "Selección de producto";
+$_["refine_plans_label"] = "Lista de planes de refinamiento";
+$_["plugin_edit_success_msg"] = "Éxito: ha editado el módulo";
+$_["show_all_plans_option"] = "Mostrar todos los planes";
+$_["select_specific_plans_option"] = "Seleccionar planes específicos";
+$_["finance_all_products_option"] = "Todos los productos";
+$_["finance_specific_products_option"] = "Financiar productos específicos";
+$_["finance_threshold_products_option"] = "Todos los productos por encima de un precio definido.";
+$_["save_label"] = "Guardar";
+$_["cancel_label"] = "Cancelar";
+$_["sort_order_label"] = "Orden de clasificación";
+$_["specify_categories_label"] = "Especificar categorías";
+$_["specify_categories_description"] = "Solo permita la financiación de los productos en las categorías especificadas";
+$_["extensions_label"] = "Extensiones";
+$_["product_selection_description"] = "Personaliza qué productos se pueden financiar";
+$_["heading_title"] = "Pagar por finanzas";
+$_["plugin_active_description"] = "Habilitar o deshabilitar la opción de financiación al finalizar la compra";
+$_["widget_footnote_label"] = "Nota";
+$_["widget_button_text_label"] = "Botón de texto";
+$_["widget_button_text_description"] = "Texto que aparece en el botón del widget";
+$_["widget_footnote_description"] = "Letra pequeña que aparece al final del widget";
+$_["enabled_option"] = "Habilitado";
+$_["disabled_option"] = "Discapacitado";
+$_["widget_mode_label"] = "Moda";
+$_["widget_mode_description"] = "Si el widget aparece como inmediatamente (calculadora) o cuando se hace clic en un botón (lightbox)";
+$_["home_label"] = "Inicio";
+$_["product_price_threshold_label"] = "Umbral de precio del producto";
+$_["product_price_threshold_description"] = "La cantidad mínima que debe costar un producto para ser viable para la financiación";
+$_["confirm_label"] = "Confirmar";
+$_["loading_label"] = "Cargando";
+$_["plugin_title"] = "Pagar por finanzas";
+
+?>
---
+++ upload/catalog/language/es-es/extension/payment/financePlugin.php
@@ -0,0 +1,9 @@
+<?php
+
+$_["save_label"] = "Guardar";
+$_["confirm_label"] = "Confirmar";
+$_["loading_label"] = "Cargando";
+$_["plugin_title"] = "Pagar por finanzas";
+$_["generic_credit_request_error"] = "No se pudo iniciar la solicitud de crédito";
+
+?>
